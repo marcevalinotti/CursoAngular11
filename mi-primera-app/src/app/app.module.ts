738f431f934a2b5246b67b5552fcdea2007f538e
marcevalinotti <marcevalinotti@gmail.com>
2021-09-10 16:09:09 -0300

    First Push

---
+++ mi-primera-app/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PersonasComponent } from './personas/personas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, PersonasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
